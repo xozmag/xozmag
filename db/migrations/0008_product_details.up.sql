274254e851c7dae884bc0ae8824bffc1bad3787f
farrux <farruxismoilov33@gmail.com>
2024-11-23 15:46:28 +0500

    product details

---
+++ db/migrations/0008_product_details.up.sql
@@ -0,0 +1,21 @@
+create table product_details(
+    id uuid not null primary key,
+    product_id uuid not null,
+    state numeric(2) not null default 1,
+    color integer,
+    weight numeric,
+    capacity numeric,
+    two_dimensional_height numeric,
+    two_dimensional_width numeric,
+    three_dimensional_height numeric,
+    three_dimensional_width numeric,
+    three_dimensional_thick numeric,
+    amount bigint not null,
+    price numeric not null,
+    discount_price numeric,
+    discount_percent numeric,
+    created_at timestamp not null default current_timestamp,
+    created_by uuid,
+    updated_at timestamp,
+    updated_by uuid
+);
